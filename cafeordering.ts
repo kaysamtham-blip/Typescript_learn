7063ea7eae8ef9daa8c13eee27a8e530ba7dc50b
kaysamtham-blip <kaysamtham@icloud.com>
2026-07-04 17:54:10 +0800

    Update cafeordering.ts

--- cafeordering.ts
+++ cafeordering.ts
@@ -1,4 +1,31 @@
-function processSearch(input:string):string(
+const processSearch(input:string|number):string => {
+ if(typeof input === string) {
+ console.log("Searching for product name: " + input)
+ }
+ else if(typeof input === number) {
+ console.log("Looking up product ID: " + input)
+ }
+}
+
+function order (mknn:string; price:number; category:string):string[] [
+{mknn:"coffee", price:5, category:"food"},
+{mknn:"burger", price:15, category:"drink"},
+{mknn:"tea", price:3, category:"drink"},
+{mknn:"pizza", price:20,category:"food"}
+]
+
+//only want to see food name
+const fooditem:string=order.filter((f)=>f.category===food)
+console.log(fooditem)
+
+//only display food and drinks name
+const allname:string=order.map((n)=>n===mknn)
+console.log(allname)
+
+//sum all orders
+const total:number=order.reduce((sum,currentp)=>sum+currentp, 0)
+console.log(total)
+
 
 1. Define the Function: Create a function named ⁠processSearch⁠ that accepts one parameter named ⁠input⁠.
 2. Apply Union Types: Set the type of ⁠input⁠ to be ⁠string | number⁠.
