82f9443024b5efbcf435ab0b3eda4aa5e19c2ccd
kaysamtham-blip <kaysamtham@icloud.com>
2026-07-06 02:42:12 +0800

    Update distributioncentre.ts

--- distributioncentre.ts
+++ distributioncentre.ts
@@ -24,3 +24,13 @@ const job = {machineID="KFK77593202", isCritical=true}
 
 processCItem(box)
 processCItem(job)
+
+
+
+//CORRECTION
+ (line15)
+- console.log("BILLING: Charging for package " + item.trackingno + "with weight " + item.weight + "kg.")
+ (line18)
+- console.log("DISPATCH: Sending item to machine " + item.machineID + ". Critical: " + item.isCritical + ".")
+ //bcs under function, we use "item" as parameter to represent the interfaces so variables under the function should add "item."
+ //interface is just a blueprint but not a data, computer do not include them in operation so you have to link it yourself with a variable in a function to define them
