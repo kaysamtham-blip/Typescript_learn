9dd7b95c0252cfc31caa6d8a2c275e076df409f4
kaysamtham-blip <kaysamtham@icloud.com>
2026-07-07 19:30:29 +0800

    Update warehousecargo.ts

--- warehousecargo.ts
+++ warehousecargo.ts
@@ -1,7 +1,24 @@
-interface Package = {
+interface Package {
 readonly trackingno: string;
 const weight?: number;
 const destination: string;
 }
 
-interface MaintenanceTask = {
+interface MaintenanceTask {
+readonly machineID: string;
+const priority: string;
+const etimatedHours;
+}
+
+type cargo = Package|MaintnanceTask;
+
+const handleCargo(item:unknown ) => {
+ if ("trackingno" in item){
+  if (item.weight!== undefined){
+  console.log("Processing package " + trackingno + "with weight: " + item.weight + "."))
+  }
+  else {
+  "Processing package " + trackingno + "with weight: unknown")
+  }
+ else if("machineID" && "priority" in item) {
+ console.log("Fixing machine " + item.machineID +"with priority: " + item.priority+ ".")
