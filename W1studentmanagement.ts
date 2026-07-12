31ae3a03e31c138a27f415c35769a1d9ba5f584d
kaysamtham-blip <kaysamtham@icloud.com>
2026-07-13 01:46:49 +0800

    Update W1studentmanagement.ts

--- W1studentmanagement.ts
+++ W1studentmanagement.ts
@@ -6,9 +6,9 @@ interface student {
   title?: string;
 }
 
-const s1:student = {name:Wini, id:253, scores:[58,76,85], isEnrolled:true}
-const s2:student = {name:alpha, id:432, scores:[73,83,75], isEnrolled:true}
-const s3:student = {name:piggie, id:229, scores:[828,90,45], isEnrolled:false}
+const s1:student = {name:"Wini", id:253, scores:[58,76,85], isEnrolled:true}
+const s2:student = {name:"Alpha", id:432, scores:[73,83,75], isEnrolled:true}
+const s3:student = {name:"Piggie", id:229, scores:[828,90,45], isEnrolled:false}
 
 const average = (scores:number[]) => {
   return (scores[0], scores[1], scores[2])/3;
@@ -56,17 +56,15 @@ wrapArray("hahaha");
 wrapArray(207);
 wrapArray(false);
 
-const findmax = (ave: result[]): string => {
-  highest = 0
-  for (let i = 0,i <=ave.length ,i=i++){
-    if(result[0] > highest){
-      student.name[i] = highest
-      return highest
-    }
-  }
-}
-
 const findmax = (detail.student[]):void => {
- const
+ const topave = average(detail.scores[0])
+ for(i=0; i<=detail.length; i++) {
+   const ave = average(detail.scores[i])
+   if(ave > topave) {
+     topave = ave
+   }
+  }
+ console.log("Top student is " + topave + ".")
+}
 
 console.log("Top student is " + highest + ".")
