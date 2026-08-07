87da451deacd71bbd66e3db54c1cc53e8b363f4a
kaysamtham-blip <kaysamtham@icloud.com>
2026-08-08 02:07:51 +0800

    Update expenses.ts

--- expenses.ts
+++ expenses.ts
@@ -3,6 +3,7 @@ type cat = "needs" | "transport" | "entertainment" | "food" | "wishlists"
 interface expense {
 name: string;
 amount: number;
+isdeferred:boolean
 date: string format: "2025-09" (year-month)
 category: cat;
 remark?: string;
@@ -11,16 +12,15 @@ remark?: string;
 const input: expense = prompt("Enter your expenses today!")
 
 const obj: expense[] = [
-{name: "ramen", amount: 5, date: "2026-07", category: "food"}
-{name: "album", amount: 207, date: "2026-02", category: "wishlists", remark: "self-reward for driving license"}
-{name: "grab", amount: 12, date: "2026-05", category: "transport", remark: "to hokkien association"}
-{name: "tissue", amount: 10, date: "2026-06", category: "needs"}
-{name: "jasmine milk tea", amount: 12,date: "2026-05", category: "entertainment"}
+{name: "ramen", amount: 5, isdeferred: false, date: "2026-07", category: "food"}
+{name: "album", amount: 207, isdeferred: true, date: "2026-02", category: "wishlists", remark: "self-reward for driving license"}
+{name: "grab", amount: 12, isdeferred: false, date: "2026-05", category: "transport", remark: "to hokkien association"}
+{name: "tissue", amount: 10, isdeferred: false, date: "2026-06", category: "needs"}
+{name: "jasmine milk tea", amount: 12, isdeferred: false, date: "2026-05", category: "entertainment"}
 ]
 
-const list = (a:expense):void => {
- console.log(obj)
- const choose = filter(a.category) {
- 
- }
+const list = (a:expense):return => {
+ return obj
+ const choose:expense = a.filter((f)=>f.category=>"needs")
+ return choose
 }
